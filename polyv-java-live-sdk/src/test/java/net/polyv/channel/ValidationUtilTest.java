891dcad1c98ccbac34e7809a842f371b9ff29d45
wujie <wujie@polyv.net>
2020-10-12 09:39:47 +0800

    JSDK-1 异常代码提交

--- polyv-java-live-sdk/src/test/java/net/polyv/channel/ValidationUtilTest.java
+++ polyv-java-live-sdk/src/test/java/net/polyv/channel/ValidationUtilTest.java
@@ -2,20 +2,21 @@ package net.polyv.channel;
 
 import org.junit.Test;
 
+import net.polyv.common.util.ValidationUtil;
 import net.polyv.live.entity.channel.LiveChannelRequest;
 
 /**
  * @author: thomas
- 
  **/
 public class ValidationUtilTest {
     
     @Test
-    public void testValidationUtil(){
+    public void testValidationUtil() {
         javax.el.ExpressionFactory factory = null;
         LiveChannelRequest liveChannelRequest = LiveChannelRequest.builder().userId("kskskskssksksk").build();
-        net.polyv.live.util.ValidationUtil.ValidResult validResult = net.polyv.live.util.ValidationUtil.validateBean(liveChannelRequest);
-        if(validResult.hasErrors()){
+        ValidationUtil.ValidResult validResult =  ValidationUtil.validateBean(
+                liveChannelRequest);
+        if (validResult.hasErrors()) {
             String errors = validResult.getErrors();
             System.out.println(errors);
         }

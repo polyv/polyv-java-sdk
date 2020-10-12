074c567cfde6e866dea54880c50f9bffafdadca5
wujie <wujie@polyv.net>
2020-10-12 09:28:20 +0800

    JSDK-1 异常代码提交

--- polyv-java-live-sdk-web/src/main/java/net/polyv/live/action/ChannelAction.java
+++ polyv-java-live-sdk-web/src/main/java/net/polyv/live/action/ChannelAction.java
@@ -1,6 +1,7 @@
 package net.polyv.live.action;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,7 +40,8 @@ public class ChannelAction {
     @ApiOperation(value = "创建一个直播频道，成功创建返回该频道的基本信息",notes = "调用示例：参考polyv-java-live-sdk单元测试ChannelTest.testCreateChannel()方法。<a target=\"_blank\"  href=\"http://47.115.173.234:3000/#/channel/channelManager?id=sdk%e9%a2%91%e9%81%93%e6%93%8d%e4%bd%9c\">频道创建</a>    ")
     @PostMapping("/createChannel")
     @ResponseBody
-    public LiveChannelResponse createChannel(LiveChannelRequest liveChannelRequest) throws IOException {
+    public LiveChannelResponse createChannel(LiveChannelRequest liveChannelRequest)
+            throws IOException, NoSuchAlgorithmException {
         LiveChannelResponse liveChannelResponse =  new LiveChannelServiceImpl().createChannel(liveChannelRequest);
         return  liveChannelResponse;
     }
@@ -53,7 +55,8 @@ public class ChannelAction {
     @ApiOperation(value = "批量创建直播频道，成功创建返回所创建频道的基本信息。留意，如果响应失败，则表示全部频道都失败，不会有部份成功、部份失败的结果",notes = "调用示例：参考polyv-java-live-sdk单元测试ChannelTest.testCreateChannelList()方法。<a target=\"_blank\"  href=\"http://47.115.173.234:3000/#/channel/channelManager?id=sdk%e9%a2%91%e9%81%93%e6%93%8d%e4%bd%9c\">频道创建</a>    ")
     @PostMapping("/createChannelList")
     @ResponseBody
-    public LiveCreateChannelListResponse createChannelList(LiveCreateChannelListRequest liveCreateChannelListRequest) throws IOException {
+    public LiveCreateChannelListResponse createChannelList(LiveCreateChannelListRequest liveCreateChannelListRequest)
+            throws IOException, NoSuchAlgorithmException {
         LiveCreateChannelListResponse liveCreateChannelListResponse =  new LiveChannelServiceImpl().createChannelList(liveCreateChannelListRequest);
         return  liveCreateChannelListResponse;
     }
@@ -67,7 +70,8 @@ public class ChannelAction {
     @ApiOperation(value = "创建并初始化频道，用于创建频道并进行相关设置。创建成功返回频道信息。",notes = "调用示例：参考polyv-java-live-sdk单元测试ChannelTest.testCreateChannelInitCode()等方法，可设置不同的观看条件。<a target=\"_blank\"  href=\"http://47.115.173.234:3000/#/channel/channelManager?id=sdk%e9%a2%91%e9%81%93%e6%93%8d%e4%bd%9c\">创建并初始化频道</a>    ")
     @PostMapping("/createChannelInit")
     @ResponseBody
-    public LiveChannelInitResponse createChannelInit(@RequestBody LiveChannelInitRequest liveChannelInitRequest) throws IOException {
+    public LiveChannelInitResponse createChannelInit(@RequestBody LiveChannelInitRequest liveChannelInitRequest)
+            throws IOException, NoSuchAlgorithmException {
         LiveChannelInitResponse liveChannelInitResponse =  new LiveChannelServiceImpl().createChannelInit(liveChannelInitRequest);
         return  liveChannelInitResponse;
     }
@@ -75,14 +79,16 @@ public class ChannelAction {
     @ApiOperation(value = "设置频道详情，成功时为true，错误时为\"\"",notes = "调用示例：参考polyv-java-live-sdk单元测试ChannelTest.testCreateChannel()方法。<a target=\"_blank\"  href=\"http://47.115.173.234:3000/#/channel/channelManager?id=sdk%e9%a2%91%e9%81%93%e6%93%8d%e4%bd%9c\">频道创建</a>    ")
     @PostMapping("/updateChannelDetail")
     @ResponseBody
-    public String updateChannelDetail(LiveChannelDetailRequest liveChannelDetailRequest) throws IOException {
+    public String updateChannelDetail(LiveChannelDetailRequest liveChannelDetailRequest)
+            throws IOException, NoSuchAlgorithmException {
         return new LiveChannelServiceImpl().updateChannelDetail(liveChannelDetailRequest);
     }
     
     @ApiOperation(value = "查询课件重制任务列表，成功时返回分页的课件重制任务列表",notes = "调用示例：参考polyv-java-live-sdk单元测试ChannelTest.testListPPTRecord()方法。<a target=\"_blank\"  href=\"http://47.115.173.234:3000/#/channel/channelManager?id=sdk%e9%a2%91%e9%81%93%e6%93%8d%e4%bd%9c\">频道创建</a>    ")
     @PostMapping("/listPPTRecord")
     @ResponseBody
-    public LiveListChannelPPTRecordResponse listPPTRecord(LiveListChannelPPTRecordRequest liveListChannelPPTRecordRequest) throws IOException {
+    public LiveListChannelPPTRecordResponse listPPTRecord(LiveListChannelPPTRecordRequest liveListChannelPPTRecordRequest)
+            throws IOException, NoSuchAlgorithmException {
         return new LiveChannelServiceImpl().listPPTRecord(liveListChannelPPTRecordRequest);
     }
 }

dbc1858b6fcf1bce4f98b7161927a46f164c5b39
wujie <wujie@polyv.net>
2020-10-19 09:51:37 +0800

    JSDK-79 查询频道问卷结果-API功能对接和开发-实体bean功能开发

---
+++ polyv-java-live-sdk/src/main/java/net/polyv/live/entity/interact/LiveQuestionResultRequest.java
@@ -0,0 +1,50 @@
+package net.polyv.live.entity.interact;
+
+import javax.validation.constraints.NotNull;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import net.polyv.live.entity.LiveCommonRequest;
+
+/**
+ * 查询频道问卷结果请求实体
+ * @author: thomas
+ **/
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel("查询频道问卷结果请求实体")
+public class LiveQuestionResultRequest extends LiveCommonRequest {
+   
+    
+    /**
+     * 频道号
+     */
+    @ApiModelProperty(name = "channelId", value = "频道号", required = true)
+    private Integer channelId;
+    
+    /**
+     * 开始时间，格式：2018-12-10
+     */
+    @ApiModelProperty(name = "startDate", value = "开始时间，格式：2018-12-10", required = false)
+    private String startDate;
+    
+    /**
+     * 结束时间，格式：2018-12-10
+     */
+    @ApiModelProperty(name = "endDate", value = "结束时间，格式：2018-12-10", required = false)
+    private String endDate;
+    
+    /**
+     * 问卷ID，和startDate/endDate同时提交时，会优先以该参数查询
+     */
+    @ApiModelProperty(name = "questionnaireId", value = "问卷ID，和startDate/endDate同时提交时，会优先以该参数查询", required = false)
+    private String questionnaireId;
+}
---
+++ polyv-java-live-sdk/src/main/java/net/polyv/live/entity/interact/LiveQuestionResultResponse.java
@@ -0,0 +1,21 @@
+package net.polyv.live.entity.interact;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * 查询频道问卷结果响应实体
+ * @author: thomas
+ **/
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+//@NoArgsConstructor
+@Builder
+@ApiModel("查询频道问卷结果响应实体")
+public class LiveQuestionResultResponse {
+}

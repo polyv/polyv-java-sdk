3fa5c9966a0e52ff105443ff7d0af77c71310add
sadboy <wangzhikang@polyv.net>
2020-10-19 16:53:43 +0800

    JSDK-83 完成查询频道实时在线人数实体创建

---
+++ polyv-java-live-sdk/src/main/java/net/polyv/live/entity/channel/LiveChannelViewerCountRequest.java
@@ -0,0 +1,38 @@
+package net.polyv.live.entity.channel;
+
+import javax.validation.constraints.NotNull;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import net.polyv.live.entity.LiveCommonRequest;
+
+/**
+ * 查询频道实时在线人数请求实体
+ * @author: sadboy
+ **/
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel("查询频道实时在线人数请求实体")
+public class LiveChannelViewerCountRequest extends LiveCommonRequest {
+    
+    /**
+     * POLYV用户ID，通过注册保利威官网获取，路径：官网->登录->直播（开发设置）
+     */
+    @ApiModelProperty(hidden = true)
+    @NotNull(message = "属性userId不能为空")
+    private String userId;
+    
+    /**
+     * 频道号
+     */
+    @ApiModelProperty(name = "channelId", value = "频道号", required = true)
+    @NotNull(message = "channelId不能为空")
+    private Integer channelId;
+    
+}

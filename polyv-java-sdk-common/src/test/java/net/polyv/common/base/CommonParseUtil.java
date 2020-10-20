3cf7d339d3d23267a8429091c882d9c4792fcfc8
sadboy <wangzhikang@polyv.net>
2020-10-20 15:52:06 +0800

    JSDK-2 添加Int为空添加验证代码

--- polyv-java-sdk-common/src/test/java/net/polyv/common/base/CommonParseUtil.java
+++ polyv-java-sdk-common/src/test/java/net/polyv/common/base/CommonParseUtil.java
@@ -30,30 +30,31 @@ public class CommonParseUtil {
     
     public static void testCreateFields() {
         Boolean req = null;//判断是生成请求还是响应
-        String fieldsStr = "code\tint\t响应代码，成功为200，失败为400，签名错误为403，异常错误500\n" +
-                "status\tstring\t成功为success，失败为error\n" + "message\tstring\t错误时为错误提示消息\n" +
-                "questionnaireId\tint\t问卷ID\n" + "customQuestionnaireId\tint\t用户自定义问卷ID\n" + "channelId\tint\t频道ID\n" +
-                "userId\tint\t用户ID\n" + "name\tstring\t问卷名称\n" + "status\tstring\t问卷状态\n" +
-                "createdTime\tdate\t问卷创建时间\n" + "endTime\tdate\t停止问卷时间\n" + "questions\tstring\t问卷问题列表\n" + " \n" +
-                "questionId\tint\t问题ID\n" + "name\tstring\t问题题目\n" + "type\tstring\t问题类型，取值：R 单选；C 多选；S 评星题；Q 问答\n" +
-                "option1~10\tstring\t选项A~G 的内容\n" + "createdTime\tdate\t创建时间\n" + "lastModified\tdate\t最后修改时间\n" +
-                "scoreEnabled\tstring\t是否计分，取值Y、N\n" + "score\tint\t题目分值\n" + "required\tstring\t是否必填，取值Y、N\n" +
-                "answer\tstring\t问题答案";
-
-         fieldsStr = "contents\tstring\t查询的结果列表\n" + "questionnaireId\tstring\t问卷ID\n" + "channelId\tint\t频道ID\n" +
-                 "userId\tint\t用户ID\n" + "name\tstring\t问卷名称\n" +
-                 "status\tstring\t问卷状态，取值：saved(已保存)，published （已发布），forbidden （问卷已完成填写）\n" +
-                 "createdTime\tlong\t问卷创建时间\n" + "lastModified\tlong\t问卷最后修改时间\n" + "endTime\tlong\t停止提交问卷时间\n" +
-                 "customQuestionnaireId\tstring\t用户自定义问卷ID";
-    
+        String fieldsStr =
+                "code\tint\t响应代码，成功为200，失败为400，签名错误为403，异常错误500\n" + "status\tstring\t成功为success，失败为error\n" +
+                        "message\tstring\t错误时为错误提示消息\n" + "questionnaireId\tint\t问卷ID\n" +
+                        "customQuestionnaireId\tint\t用户自定义问卷ID\n" + "channelId\tint\t频道ID\n" + "userId\tint\t用户ID\n" +
+                        "name\tstring\t问卷名称\n" + "status\tstring\t问卷状态\n" + "createdTime\tdate\t问卷创建时间\n" +
+                        "endTime\tdate\t停止问卷时间\n" + "questions\tstring\t问卷问题列表\n" + " \n" + "questionId\tint\t问题ID\n" +
+                        "name\tstring\t问题题目\n" + "type\tstring\t问题类型，取值：R 单选；C 多选；S 评星题；Q 问答\n" +
+                        "option1~10\tstring\t选项A~G 的内容\n" + "createdTime\tdate\t创建时间\n" +
+                        "lastModified\tdate\t最后修改时间\n" + "scoreEnabled\tstring\t是否计分，取值Y、N\n" + "score\tint\t题目分值\n" +
+                        "required\tstring\t是否必填，取值Y、N\n" + "answer\tstring\t问题答案";
+        
+        fieldsStr = "contents\tstring\t查询的结果列表\n" + "questionnaireId\tstring\t问卷ID\n" + "channelId\tint\t频道ID\n" +
+                "userId\tint\t用户ID\n" + "name\tstring\t问卷名称\n" +
+                "status\tstring\t问卷状态，取值：saved(已保存)，published （已发布），forbidden （问卷已完成填写）\n" +
+                "createdTime\tlong\t问卷创建时间\n" + "lastModified\tlong\t问卷最后修改时间\n" + "endTime\tlong\t停止提交问卷时间\n" +
+                "customQuestionnaireId\tstring\t用户自定义问卷ID";
+        
         fieldsStr = "\n" + "\n" + "channelId\t是\tstring\t频道号\n" + "questionnaireId\t否\tstring\t问卷id,修改问卷时需要\n" +
-                 "customQuestionnaireId\t否\tstring\t客户自定义问卷id\n" + "questionnaireTitle\t是\tstring\t问卷标题\n" +
-                 "questions\t是\tarray\t题目数组\n" + "questionId\t否\tstring\t题目id，修改问卷时需要传\n" + "name\t是\tstring\t题目\n" +
-                 "type\t是\tstring\t题目类型,R为单选，C为多选，Q为问答\n" + "scoreEnabled\t否\tstring\t题目是否需要评分，Y为需要，N为不需要\n" +
-                 "answer\t否\tstring\t需要评分的选择题才有答案，填入对应选项序号，如：A或AB\n" + "required\t否\tstring\t题目是否为必答，Y为必答，N为非必答\n" +
-                 "options\t否\tarray\t题目为单选题或多选题为必填，选项数组下标0-9对应答案A-J\n" + "options[]\t否\tstring\t选项描述\n" + "\n" + " ";
-
-        fieldsStr="questionnaireId\tstring\t问卷id\n" + "questionIds[]\tarray\t题目的id数组";
+                "customQuestionnaireId\t否\tstring\t客户自定义问卷id\n" + "questionnaireTitle\t是\tstring\t问卷标题\n" +
+                "questions\t是\tarray\t题目数组\n" + "questionId\t否\tstring\t题目id，修改问卷时需要传\n" + "name\t是\tstring\t题目\n" +
+                "type\t是\tstring\t题目类型,R为单选，C为多选，Q为问答\n" + "scoreEnabled\t否\tstring\t题目是否需要评分，Y为需要，N为不需要\n" +
+                "answer\t否\tstring\t需要评分的选择题才有答案，填入对应选项序号，如：A或AB\n" + "required\t否\tstring\t题目是否为必答，Y为必答，N为非必答\n" +
+                "options\t否\tarray\t题目为单选题或多选题为必填，选项数组下标0-9对应答案A-J\n" + "options[]\t否\tstring\t选项描述\n" + "\n" + " ";
+        
+        fieldsStr = "questionnaireId\tstring\t问卷id\n" + "questionIds[]\tarray\t题目的id数组";
         //        fieldsStr = readFileString("C:\\Users\\T460\\Desktop\\fields.txt");
         
         String[] lineStr = fieldsStr.split("\n");
@@ -62,7 +63,9 @@ public class CommonParseUtil {
             temp = temp.trim();
             temp = temp.replaceAll("\\s+", "\t");
             String[] lineArr = temp.split("\t");
-            if(lineArr.length<2) continue;
+            if (lineArr.length < 2) {
+                continue;
+            }
             if (req == null) {
                 req = lineArr.length == 4;
             }
@@ -78,7 +81,7 @@ public class CommonParseUtil {
                 descript = lineArr[2].trim();
             }
             
-            String reallyType = getReallyType(fieldType , req);
+            String reallyType = getReallyType(fieldType, req);
             
             System.out.println("/**");
             System.out.println("* " + descript);
@@ -87,10 +90,10 @@ public class CommonParseUtil {
                     "@ApiModelProperty(name = \"" + fieldName + "\", value = \"" + descript + "\", required = " +
                             mustSelect + ")");
             System.out.println();
-            if ("string".equals(fieldType) && "true".equals(mustSelect)) {
+            if (("string".equals(fieldType) || "int".equals(fieldType)) && "true".equals(mustSelect)) {
                 System.out.println("@NotNull(message = \"" + fieldName + "不能为空\")");
             }
-            if ("timestamp".equals(fieldType) ) {
+            if ("timestamp".equals(fieldType)) {
                 System.out.print("@JSONField(format = \"yyyy-MM-dd hh:mm:ss\")");
             }
             System.out.print("private " + reallyType + " " + fieldName + ";");
@@ -99,7 +102,7 @@ public class CommonParseUtil {
         }
     }
     
-    private static String getReallyType(String fieldType,boolean isRequest) {
+    private static String getReallyType(String fieldType, boolean isRequest) {
         switch (fieldType.toLowerCase()) {
             case "integer":
             case "int":
@@ -112,10 +115,11 @@ public class CommonParseUtil {
                 return "Long";
             case "timestamp":
             case "date":
-                if(isRequest)
+                if (isRequest) {
                     return "Long";
-                else
+                } else {
                     return "Date";
+                }
             case "float":
                 return "Float";
             case "array":
@@ -145,7 +149,5 @@ public class CommonParseUtil {
         return stringBuffer.toString();
     }
     
-    
-  
     
 }

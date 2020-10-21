532e17c793111fdc8c4f8d4a81b72723dd404ef8
wujie <wujie@polyv.net>
2020-10-21 10:04:21 +0800

    JSDK-1  点播密钥更换

--- polyv-java-vod-sdk-web/src/main/java/net/polyv/vod/config/StartupListener.java
+++ polyv-java-vod-sdk-web/src/main/java/net/polyv/vod/config/StartupListener.java
@@ -17,14 +17,18 @@ public class StartupListener implements ApplicationContextAware   {
     
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        String userId = "1b448be323";
-        String writeToken = "f9810825-7512-476d-95ec-9ff2968df5de";
-        String readToken = "6e26de86-57f1-436d-8b3f-95ff69c971f7";
-        String secretKey = "8eVs9NVrNm";
-        String secretKeyBak = "VjmWPuCTpE";
-        VodGlobalConfig.init(userId, writeToken, readToken, secretKey);
-        log.info("--初始化完成--");
+        initVodPolyv();
     }
+    
+    private void initVodPolyv(){
+        String userId = "1b448be323";
+        String writeToken = "s2GUm9YXzWpU1Z6-Uagx0rs3oG0QNZQq";
+        String readToken = "WmBI70idfN-4Sb9qfRot7-gqkkzWcCRG";
+        String secretKey = "GyqkTJckCT";
+        VodGlobalConfig.init(userId, writeToken, readToken, secretKey);
+        log.debug("--初始化完成--");
+    }
+    
     
     
 }

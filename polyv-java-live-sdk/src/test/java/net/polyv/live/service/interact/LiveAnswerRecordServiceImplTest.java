614a5f70f4e9fa0d8f2bb547110871cac34800b0
wujie <wujie@polyv.net>
2020-10-26 17:43:08 +0800

    JSDK-1     基础代码提交

--- polyv-java-live-sdk/src/test/java/net/polyv/live/service/interact/LiveAnswerRecordServiceImplTest.java
+++ polyv-java-live-sdk/src/test/java/net/polyv/live/service/interact/LiveAnswerRecordServiceImplTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import com.alibaba.fastjson.JSON;
 
 import lombok.extern.slf4j.Slf4j;
+import net.polyv.common.exception.PloyvSdkException;
 import net.polyv.live.entity.interact.LiveQuestionAnswerRecordRequest;
 import net.polyv.live.entity.interact.LiveQuestionAnswerRecordResponse;
 import net.polyv.live.service.BaseTest;
@@ -30,13 +31,23 @@ public class LiveAnswerRecordServiceImplTest extends BaseTest {
     public void testGetAnswerRecord() throws IOException, NoSuchAlgorithmException {
         LiveQuestionAnswerRecordRequest liveQuestionAnswerRecordRequest = new LiveQuestionAnswerRecordRequest();
         List<LiveQuestionAnswerRecordResponse> liveCheckinResponse = null;
-        Integer channelId = super.createChannel();
-        liveQuestionAnswerRecordRequest.setChannelId(channelId).setRequestId(LiveSignUtil.generateUUID());
-        liveCheckinResponse = new LiveAnswerRecordServiceImpl().getAnswerRecord(liveQuestionAnswerRecordRequest);
-        Assert.assertNotNull(liveCheckinResponse);
-        if (liveCheckinResponse != null) {
-            //to do something ......
-            log.debug("测试查询频道答题卡答题结果成功{}", JSON.toJSONString(liveCheckinResponse));
+        try {
+            Integer channelId = super.createChannel();
+            liveQuestionAnswerRecordRequest.setChannelId(channelId).setRequestId(LiveSignUtil.generateUUID());
+            liveCheckinResponse = new LiveAnswerRecordServiceImpl().getAnswerRecord(liveQuestionAnswerRecordRequest);
+            Assert.assertNotNull(liveCheckinResponse);
+            if (liveCheckinResponse != null) {
+                //to do something ......
+                log.debug("测试查询频道答题卡答题结果成功{}", JSON.toJSONString(liveCheckinResponse));
+            }
+        } catch (PloyvSdkException e) {
+            //参数校验不合格 或者 请求服务器端500错误，错误信息见PloyvSdkException.getMessage(),B
+            log.error(e.getMessage(), e);
+            // 异常返回做B端异常的业务逻辑，记录log 或者 上报到ETL 或者回滚事务
+            throw e;
+        } catch (Exception e) {
+            log.error("SDK调用异常", e);
+            throw e;
         }
     }
 }
--- polyv-java-live-sdk/src/test/java/net/polyv/live/service/interact/LiveQuestionnaireServiceImplTest.java
+++ polyv-java-live-sdk/src/test/java/net/polyv/live/service/interact/LiveQuestionnaireServiceImplTest.java
@@ -11,6 +11,7 @@ import com.alibaba.fastjson.JSON;
 
 import edu.emory.mathcs.backport.java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
+import net.polyv.common.exception.PloyvSdkException;
 import net.polyv.live.constant.LiveConstant;
 import net.polyv.live.entity.interact.LiveQuestionnaireDetailRequest;
 import net.polyv.live.entity.interact.LiveQuestionnaireDetailResponse;
@@ -40,57 +41,68 @@ public class LiveQuestionnaireServiceImplTest extends BaseTest {
     @Test
     public void testSetQuestionnaireDetailInfo() throws IOException, NoSuchAlgorithmException {
         LiveQuestionnaireDetailSetRequest liveQuestionnaireDetailSetRequest = new LiveQuestionnaireDetailSetRequest();
-        LiveQuestionnaireDetailSetResponse liveQuestionnaireDetailSetResponse =null;
-        Integer channelId = super.createChannel();
-        //封装问卷请求对象
-        liveQuestionnaireDetailSetRequest.setChannelId(channelId).setCustomQuestionnaireId(LiveSignUtil.generateUUID())
+        LiveQuestionnaireDetailSetResponse liveQuestionnaireDetailSetResponse = null;
+        try {
+            Integer channelId = super.createChannel();
+            //封装问卷请求对象
+            liveQuestionnaireDetailSetRequest.setChannelId(channelId)
+                    .setCustomQuestionnaireId(LiveSignUtil.generateUUID())
 //                .setQuestionnaireId("fs9skpv22f")
-                .setQuestionnaireTitle("测试试卷，明天会更好调查2").setRequestId(LiveSignUtil.generateUUID());
-        
-        //封装问卷题目
-        LiveQuestionnaireDetailSetRequest.QuestionDetail questionDetail =
-                liveQuestionnaireDetailSetRequest.new QuestionDetail();
-        questionDetail.setQuestionId(LiveSignUtil.generateUUID())
-                .setName("您的兴趣爱好？")
-                .setAnswer("A")
-                .setScoreEnabled(LiveConstant.Flag.YES.getFlag())
-                .setRequired(LiveConstant.Flag.YES.getFlag())
-                .setOptions(Arrays.asList(new String[]{"篮球", "足球", "排球", "跑步","羽毛球"}))
-                .setScore(20)
-                .setType(LiveConstant.QuestionType.CHECK.getType());
-        
-        LiveQuestionnaireDetailSetRequest.QuestionDetail questionDetail1 =
-                liveQuestionnaireDetailSetRequest.new QuestionDetail();
-        questionDetail1.setQuestionId(LiveSignUtil.generateUUID())
-                .setName("您的性别")
-                .setScoreEnabled(LiveConstant.Flag.NO.getFlag())
-                .setRequired(LiveConstant.Flag.YES.getFlag())
-                .setOptions(Arrays.asList(new String[]{"M", "W"}))
-                .setType(LiveConstant.QuestionType.RADIO.getType());
-        
-        LiveQuestionnaireDetailSetRequest.QuestionDetail questionDetail2 =
-                liveQuestionnaireDetailSetRequest.new QuestionDetail();
-        questionDetail2.setQuestionId(LiveSignUtil.generateUUID())
-                .setName("您的职务？")
-                .setScoreEnabled(LiveConstant.Flag.NO.getFlag())
-                .setRequired(LiveConstant.Flag.YES.getFlag())
-                .setType(LiveConstant.QuestionType.QUESTION.getType());
-        
-        //将问卷题目和问卷关联
-        liveQuestionnaireDetailSetRequest.setQuestions(Arrays.asList(
-                new LiveQuestionnaireDetailSetRequest.QuestionDetail[]{questionDetail, questionDetail1,
-                        questionDetail2}));
-        
-        //发送请求
-          liveQuestionnaireDetailSetResponse =
-                new LiveQuestionnaireServiceImpl().setQuestionnaireDetailInfo(
-                        liveQuestionnaireDetailSetRequest);
-        
-        //判断结果
-        Assert.assertNotNull(liveQuestionnaireDetailSetResponse);
-        if (liveQuestionnaireDetailSetResponse != null) {
-            //to do something ......
-            log.debug("测试添加频道问卷成功{}", JSON.toJSONString(liveQuestionnaireDetailSetResponse));
+                    .setQuestionnaireTitle("测试试卷，明天会更好调查2")
+                    .setRequestId(LiveSignUtil.generateUUID());
+            
+            //封装问卷题目
+            LiveQuestionnaireDetailSetRequest.QuestionDetail questionDetail =
+                    liveQuestionnaireDetailSetRequest.new QuestionDetail();
+            questionDetail.setQuestionId(LiveSignUtil.generateUUID())
+                    .setName("您的兴趣爱好？")
+                    .setAnswer("A")
+                    .setScoreEnabled(LiveConstant.Flag.YES.getFlag())
+                    .setRequired(LiveConstant.Flag.YES.getFlag())
+                    .setOptions(Arrays.asList(new String[]{"篮球", "足球", "排球", "跑步", "羽毛球"}))
+                    .setScore(20)
+                    .setType(LiveConstant.QuestionType.CHECK.getType());
+            
+            LiveQuestionnaireDetailSetRequest.QuestionDetail questionDetail1 =
+                    liveQuestionnaireDetailSetRequest.new QuestionDetail();
+            questionDetail1.setQuestionId(LiveSignUtil.generateUUID())
+                    .setName("您的性别")
+                    .setScoreEnabled(LiveConstant.Flag.NO.getFlag())
+                    .setRequired(LiveConstant.Flag.YES.getFlag())
+                    .setOptions(Arrays.asList(new String[]{"M", "W"}))
+                    .setType(LiveConstant.QuestionType.RADIO.getType());
+            
+            LiveQuestionnaireDetailSetRequest.QuestionDetail questionDetail2 =
+                    liveQuestionnaireDetailSetRequest.new QuestionDetail();
+            questionDetail2.setQuestionId(LiveSignUtil.generateUUID())
+                    .setName("您的职务？")
+                    .setScoreEnabled(LiveConstant.Flag.NO.getFlag())
+                    .setRequired(LiveConstant.Flag.YES.getFlag())
+                    .setType(LiveConstant.QuestionType.QUESTION.getType());
+            
+            //将问卷题目和问卷关联
+            liveQuestionnaireDetailSetRequest.setQuestions(Arrays.asList(
+                    new LiveQuestionnaireDetailSetRequest.QuestionDetail[]{questionDetail, questionDetail1,
+                            questionDetail2}));
+            
+            //发送请求
+            liveQuestionnaireDetailSetResponse = new LiveQuestionnaireServiceImpl().setQuestionnaireDetailInfo(
+                    liveQuestionnaireDetailSetRequest);
+            
+            //判断结果
+            Assert.assertNotNull(liveQuestionnaireDetailSetResponse);
+            if (liveQuestionnaireDetailSetResponse != null) {
+                //to do something ......
+                log.debug("测试添加频道问卷成功{}", JSON.toJSONString(liveQuestionnaireDetailSetResponse));
+            }
+        } catch (PloyvSdkException e) {
+            //参数校验不合格 或者 请求服务器端500错误，错误信息见PloyvSdkException.getMessage(),B
+            log.error(e.getMessage(), e);
+            // 异常返回做B端异常的业务逻辑，记录log 或者 上报到ETL 或者回滚事务
+            throw e;
+        } catch (Exception e) {
+            log.error("SDK调用异常", e);
+            throw e;
         }
         
     }
@@ -103,16 +115,26 @@ public class LiveQuestionnaireServiceImplTest extends BaseTest {
     @Test
     public void testGetQuestionnaireListInfo() throws IOException, NoSuchAlgorithmException {
         LiveQuestionnaireListRequest liveQuestionnaireListRequest = new LiveQuestionnaireListRequest();
-        LiveQuestionnaireListResponse liveQuestionnaireListResponse =null;
-        Integer channelId = super.createChannel();
-       
-        liveQuestionnaireListRequest.setChannelId(channelId).setRequestId(LiveSignUtil.generateUUID());
-          liveQuestionnaireListResponse = new LiveQuestionnaireServiceImpl().getQuestionnaireListInfo(
-                liveQuestionnaireListRequest);
-        Assert.assertNotNull(liveQuestionnaireListResponse);
-        if (liveQuestionnaireListResponse != null) {
-            //to do something ......
-            log.debug("测试查询频道问卷列表成功{}", JSON.toJSONString(liveQuestionnaireListResponse));
+        LiveQuestionnaireListResponse liveQuestionnaireListResponse = null;
+        try {
+            Integer channelId = super.createChannel();
+            
+            liveQuestionnaireListRequest.setChannelId(channelId).setRequestId(LiveSignUtil.generateUUID());
+            liveQuestionnaireListResponse = new LiveQuestionnaireServiceImpl().getQuestionnaireListInfo(
+                    liveQuestionnaireListRequest);
+            Assert.assertNotNull(liveQuestionnaireListResponse);
+            if (liveQuestionnaireListResponse != null) {
+                //to do something ......
+                log.debug("测试查询频道问卷列表成功{}", JSON.toJSONString(liveQuestionnaireListResponse));
+            }
+        } catch (PloyvSdkException e) {
+            //参数校验不合格 或者 请求服务器端500错误，错误信息见PloyvSdkException.getMessage(),B
+            log.error(e.getMessage(), e);
+            // 异常返回做B端异常的业务逻辑，记录log 或者 上报到ETL 或者回滚事务
+            throw e;
+        } catch (Exception e) {
+            log.error("SDK调用异常", e);
+            throw e;
         }
     }
     
@@ -125,18 +147,27 @@ public class LiveQuestionnaireServiceImplTest extends BaseTest {
     public void testGetQuestionnaireDetailInfo() throws IOException, NoSuchAlgorithmException {
         LiveQuestionnaireDetailRequest liveQuestionnaireDetailRequest = new LiveQuestionnaireDetailRequest();
         LiveQuestionnaireDetailResponse liveQuestionnaireDetailResponse = null;
-        Integer channelId = super.createChannel();
-        //获取详情
-        liveQuestionnaireDetailRequest.setChannelId(channelId)
-                .setQuestionnaireId("fs9skpv22f")
-                .setRequestId(LiveSignUtil.generateUUID());
-          liveQuestionnaireDetailResponse =
-                new LiveQuestionnaireServiceImpl().getQuestionnaireDetailInfo(
-                liveQuestionnaireDetailRequest);
-        Assert.assertNotNull(liveQuestionnaireDetailResponse);
-        if (liveQuestionnaireDetailResponse != null) {
-            //to do something ......
-            log.debug("测试查询频道问卷详情成功{}", JSON.toJSONString(liveQuestionnaireDetailResponse));
+        try {
+            Integer channelId = super.createChannel();
+            //获取详情
+            liveQuestionnaireDetailRequest.setChannelId(channelId)
+                    .setQuestionnaireId("fs9skpv22f")
+                    .setRequestId(LiveSignUtil.generateUUID());
+            liveQuestionnaireDetailResponse = new LiveQuestionnaireServiceImpl().getQuestionnaireDetailInfo(
+                    liveQuestionnaireDetailRequest);
+            Assert.assertNotNull(liveQuestionnaireDetailResponse);
+            if (liveQuestionnaireDetailResponse != null) {
+                //to do something ......
+                log.debug("测试查询频道问卷详情成功{}", JSON.toJSONString(liveQuestionnaireDetailResponse));
+            }
+        } catch (PloyvSdkException e) {
+            //参数校验不合格 或者 请求服务器端500错误，错误信息见PloyvSdkException.getMessage(),B
+            log.error(e.getMessage(), e);
+            // 异常返回做B端异常的业务逻辑，记录log 或者 上报到ETL 或者回滚事务
+            throw e;
+        } catch (Exception e) {
+            log.error("SDK调用异常", e);
+            throw e;
         }
     }
     
@@ -150,16 +181,26 @@ public class LiveQuestionnaireServiceImplTest extends BaseTest {
     public void testGetQuestionnaireResultInfo() throws IOException, NoSuchAlgorithmException {
         LiveQuestionnaireResultRequest liveQuestionnaireResultRequest = new LiveQuestionnaireResultRequest();
         List<LiveQuestionnaireResultResponse> liveQuestionnaireResultResponse = null;
-        Integer channelId = super.createChannel();
-        
-        liveQuestionnaireResultRequest.setChannelId(channelId).setRequestId(LiveSignUtil.generateUUID());
+        try {
+            Integer channelId = super.createChannel();
+            
+            liveQuestionnaireResultRequest.setChannelId(channelId).setRequestId(LiveSignUtil.generateUUID());
 //        liveQuestionnaireResultRequest.setQuestionnaireId("fs9skpv22f");
-        liveQuestionnaireResultResponse = new LiveQuestionnaireServiceImpl().getQuestionnaireResultInfo(
-                liveQuestionnaireResultRequest);
-        Assert.assertNotNull(liveQuestionnaireResultResponse);
-        if (liveQuestionnaireResultResponse != null) {
-            //to do something ......
-            log.debug("测试查询频道问卷结果成功{}", JSON.toJSONString(liveQuestionnaireResultResponse));
+            liveQuestionnaireResultResponse = new LiveQuestionnaireServiceImpl().getQuestionnaireResultInfo(
+                    liveQuestionnaireResultRequest);
+            Assert.assertNotNull(liveQuestionnaireResultResponse);
+            if (liveQuestionnaireResultResponse != null) {
+                //to do something ......
+                log.debug("测试查询频道问卷结果成功{}", JSON.toJSONString(liveQuestionnaireResultResponse));
+            }
+        } catch (PloyvSdkException e) {
+            //参数校验不合格 或者 请求服务器端500错误，错误信息见PloyvSdkException.getMessage(),B
+            log.error(e.getMessage(), e);
+            // 异常返回做B端异常的业务逻辑，记录log 或者 上报到ETL 或者回滚事务
+            throw e;
+        } catch (Exception e) {
+            log.error("SDK调用异常", e);
+            throw e;
         }
     }
     
@@ -170,16 +211,30 @@ public class LiveQuestionnaireServiceImplTest extends BaseTest {
      */
     @Test
     public void testGetQuestionnaireResultPageInfo() throws IOException, NoSuchAlgorithmException {
-        LiveQuestionnaireResultPageRequest liveQuestionnaireResultPageRequest = new LiveQuestionnaireResultPageRequest();
+        LiveQuestionnaireResultPageRequest liveQuestionnaireResultPageRequest =
+                new LiveQuestionnaireResultPageRequest();
         LiveQuestionnaireResultPageResponse liveQuestionnaireResultPageResponse = null;
-        Integer channelId = super.createChannel();
-        liveQuestionnaireResultPageRequest.setChannelId(channelId).setPageSize(20).setCurrentPage(1).setRequestId(LiveSignUtil.generateUUID());
-        liveQuestionnaireResultPageResponse = new LiveQuestionnaireServiceImpl().getQuestionnaireResultPageInfo(
-                liveQuestionnaireResultPageRequest);
-        Assert.assertNotNull(liveQuestionnaireResultPageRequest);
-        if (liveQuestionnaireResultPageResponse != null) {
-            //to do something ......
-            log.debug("测试分页查询频道问卷结果成功{}", JSON.toJSONString(liveQuestionnaireResultPageResponse));
+        try {
+            Integer channelId = super.createChannel();
+            liveQuestionnaireResultPageRequest.setChannelId(channelId)
+                    .setPageSize(20)
+                    .setCurrentPage(1)
+                    .setRequestId(LiveSignUtil.generateUUID());
+            liveQuestionnaireResultPageResponse = new LiveQuestionnaireServiceImpl().getQuestionnaireResultPageInfo(
+                    liveQuestionnaireResultPageRequest);
+            Assert.assertNotNull(liveQuestionnaireResultPageRequest);
+            if (liveQuestionnaireResultPageResponse != null) {
+                //to do something ......
+                log.debug("测试分页查询频道问卷结果成功{}", JSON.toJSONString(liveQuestionnaireResultPageResponse));
+            }
+        } catch (PloyvSdkException e) {
+            //参数校验不合格 或者 请求服务器端500错误，错误信息见PloyvSdkException.getMessage(),B
+            log.error(e.getMessage(), e);
+            // 异常返回做B端异常的业务逻辑，记录log 或者 上报到ETL 或者回滚事务
+            throw e;
+        } catch (Exception e) {
+            log.error("SDK调用异常", e);
+            throw e;
         }
     }
 }

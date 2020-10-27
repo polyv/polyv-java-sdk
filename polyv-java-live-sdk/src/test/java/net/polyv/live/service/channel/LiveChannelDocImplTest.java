769c4519f456170e15b885774496dfbdca586f3b
sadboy <wangzhikang@polyv.net>
2020-10-27 11:24:07 +0800

    JSDK-85 完成上传频道文档

--- polyv-java-live-sdk/src/test/java/net/polyv/live/service/channel/LiveChannelDocImplTest.java
+++ polyv-java-live-sdk/src/test/java/net/polyv/live/service/channel/LiveChannelDocImplTest.java
@@ -1,5 +1,6 @@
 package net.polyv.live.service.channel;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
@@ -10,6 +11,8 @@ import com.alibaba.fastjson.JSON;
 
 import lombok.extern.slf4j.Slf4j;
 import net.polyv.live.entity.channel.doc.LiveChannelDocStatusRequest;
+import net.polyv.live.entity.channel.doc.LiveCreateChannelDocRequest;
+import net.polyv.live.entity.channel.doc.LiveCreateChannelDocResponse;
 import net.polyv.live.entity.channel.doc.LiveDeleteChannelDocRequest;
 import net.polyv.live.entity.channel.doc.LiveListChannelDocRequest;
 import net.polyv.live.entity.channel.doc.LiveListChannelDocResponse;
@@ -30,10 +33,11 @@ public class LiveChannelDocImplTest extends BaseTest {
      */
     @Test
     public void testListChannelDoc() throws IOException, NoSuchAlgorithmException {
-        Integer channelId = createChannel();
         LiveListChannelDocRequest liveListChannelDocRequest = new LiveListChannelDocRequest();
+        LiveListChannelDocResponse liveListChannelDocResponse;
+        Integer channelId = createChannel();
         liveListChannelDocRequest.setChannelId(channelId).setStatus(null);
-        LiveListChannelDocResponse liveListChannelDocResponse = new LiveChannelDocServiceImpl().listChannelDoc(
+        liveListChannelDocResponse = new LiveChannelDocServiceImpl().listChannelDoc(
                 liveListChannelDocRequest);
         Assert.assertNotNull(liveListChannelDocResponse);
         if (liveListChannelDocResponse != null) {
@@ -50,8 +54,8 @@ public class LiveChannelDocImplTest extends BaseTest {
      */
     @Test
     public void testChannelDocStatus() throws IOException, NoSuchAlgorithmException {
-        Integer channelId = createChannel();
         LiveChannelDocStatusRequest liveChannelDocStatusRequest = new LiveChannelDocStatusRequest();
+        Integer channelId = createChannel();
         liveChannelDocStatusRequest.setChannelId(channelId)
                 .setFileId(
                         "c2d585857870f4eff024976e3a265c0b1965681common,6e0603f6c8ec6113b87f69a7191d22021965681common");
@@ -75,7 +79,30 @@ public class LiveChannelDocImplTest extends BaseTest {
         Assert.assertNotNull(liveDeleteChannelDocResponse);
         if (liveDeleteChannelDocResponse != null) {
             //to do something ......
-            log.debug("测试删除频道文档，{}", liveDeleteChannelDocResponse);
+            log.debug("测试删除频道文档成功，{}", liveDeleteChannelDocResponse);
+        }
+    }
+    
+    /**
+     * 测试上传频道文档
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     */
+//    @Test
+    public void testCreateChannelDoc() throws IOException, NoSuchAlgorithmException {
+        LiveCreateChannelDocRequest liveCreateChannelDocRequest = new LiveCreateChannelDocRequest();
+        LiveCreateChannelDocResponse liveCreateChannelDocResponse;
+        File file = new File("C:\\Users\\T460\\Desktop\\葵花宝典PPT.pptx");
+        liveCreateChannelDocRequest.setChannelId(createChannel())
+                .setType("common")
+                .setFile(file)
+                .setDocName("葵花宝典")
+                .setCallbackUrl("http://www.baidu.com/callback");
+        liveCreateChannelDocResponse = new LiveChannelDocServiceImpl().createChannelDoc(liveCreateChannelDocRequest);
+        Assert.assertNotNull(liveCreateChannelDocResponse);
+        if (liveCreateChannelDocResponse != null) {
+            //to do something ......
+            log.debug("测试上传频道文档成功，{}", liveCreateChannelDocResponse);
         }
     }
     

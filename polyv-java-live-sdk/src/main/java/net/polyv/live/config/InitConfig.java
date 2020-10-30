024ee1b80dce8beb1e9362714996ab2f43454471
wujie <wujie@polyv.net>
2020-10-30 18:07:12 +0800

    JSDK-1  基础代码提交

--- polyv-java-live-sdk/src/main/java/net/polyv/live/config/InitConfig.java
+++ polyv-java-live-sdk/src/main/java/net/polyv/live/config/InitConfig.java
@@ -13,9 +13,9 @@ public class InitConfig {
      * 初始化配置
      */
     public static void initPolyvLive(){
-        String appId = "frlr1zazn3";
-        String userId = "1b448be323";
-        String appSecret = "5dbfa7dacee34ae6a7aaef4940504139";
+        String appId = "xxx";
+        String userId = "xxx";
+        String appSecret = "xxxxx";
         LiveGlobalConfig.init(appId,userId,appSecret);
         log.debug("--初始化完成--");
     }

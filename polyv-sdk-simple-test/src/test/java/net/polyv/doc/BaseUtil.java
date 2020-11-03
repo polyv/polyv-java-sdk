ce1fbbe4b5d1ea68234c7ca1ff5019f6c8b4e681
sadboy <wangzhikang@polyv.net>
2020-11-03 15:22:15 +0800

    JSDK-2 修改文档生成英文逗号问题，将文档模板文件统一放进test/sources/doc下面进行读取

--- polyv-sdk-simple-test/src/test/java/net/polyv/doc/BaseUtil.java
+++ polyv-sdk-simple-test/src/test/java/net/polyv/doc/BaseUtil.java
@@ -1,5 +1,6 @@
 package net.polyv.doc;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,48 +42,25 @@ public class BaseUtil {
         return JUNIT_CLASS_NAME;
     }
     
-    public static String lowerFirst(String oldStr){
-        
-        char[]chars = oldStr.toCharArray();
-        
+    public static String lowerFirst(String oldStr) {
+        char[] chars = oldStr.toCharArray();
         chars[0] += 32;
-        
         return String.valueOf(chars);
-        
     }
     
-    protected static String menuMdDemo = "* 产品介绍\n" + "\n" + "  * [简介](/)\n" + "  * [全局约束](/limit)\n" +
-            "  * [快速接入](/quick_start)\n" + "  * [功能索引](/function_index)\n" + "* 频道管理\n" + "\n" + "  * [账户管理]()\n" +
-            "  * [频道操作]()\n" + "  * [直播状态]()\n" + "  * [录制回放]()\n" + "  * [观看数据]()\n" + "  * [文档管理]()\n" + "* 观看管理\n" +
-            "  \n" + "  * [快捷设置]()\n" + "  * [页面信息]()\n" + "  * [页面观看条件]()\n" + "  * [页面菜单]()\n" + "  * [页面互动]()\n" +
-            "* 直播互动\n" + "    \n" + "  * [答题卡管理]()\n" + "  * [签到管理]()\n" + "  * [问卷管理]()\n" + "  * [抽奖管理]()\n" +
-            "* 聊天室\n" + "\n" + "  * [聊天室]()\n" + "  * []()\n" + "  * []()\n" + "  * []()\n" + "  * []()\n" + "* 播放器\n" +
-            "\n" + "  * [播放器]()\n" + "* 常见问题\n" + "\n" + "  * []()\n" + "  * []()\n" + "  * []()\n" + "  * []()\n" +
-            "  * []()\n" + "  * []()";
     
-    protected static String functionMdDemo = "| 功能列表    | 子功能        | API列表                                    | 备注 " +
-            "|\n" +
-            "| ----------- | ------------- | ------------------------------------------ | ---- |\n" +
-            "| ▼账户管理    |               |                                            |      |\n" +
-            "| ▼频道管理    |               |                                            |      |\n" +
-            "|             | ▼频道操作      |                                            |      |\n" +
-            "|             | ▼直播状态      |                                            |      |\n" +
-            "|             | ▼录制回放      |                                            |      |\n" +
-            "|             | ▼观看数据      |                                            |      |\n" +
-            "|             | ▼文档管理      |                                            |      |\n" +
-            "| ▼Web观看页  |                |                                            |      |\n" +
-            "|             | ▼快捷设置      |                                            |      |\n" +
-            "|             | ▼页面信息      |                                            |      |\n" +
-            "|             | ▼页面观看条件   |                                            |      |\n" +
-            "|             | ▼页面菜单      |                                            |      |\n" +
-            "|             | ▼页面互动      |                                            |      |\n" +
-            "| ▼直播互动    |               |                                            |      |\n" +
-            "|             | ▼答题卡管理    |                                            |      |\n" +
-            "|             | ▼签到管理      |                                            |      |\n" +
-            "|             | ▼问卷管理      |                                            |      |\n" +
-            "|             | ▼抽奖管理      |                                            |      |\n" +
-            "| ▼聊天室      |               |                                            |      |\n" +
-            "| ▼直播播放器  |               |                                            |      |\n" +
-            "|             | ▼播放器        |                                            |      |";
+    protected static String menuMdDemo = "";
     
+    protected static String functionMdDemo = "";
+    
+    static {
+        String barPath = BaseUtil.class.getResource("/doc/_slidebar.md").getPath();
+        String funPath = BaseUtil.class.getResource("/doc/function_index.md").getPath();
+        try {
+            menuMdDemo = DocUtil.readFileString(barPath);
+            functionMdDemo = DocUtil.readFileString(funPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

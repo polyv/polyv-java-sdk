1e9cdda86b57d2f1f3268e291985027e139a1cd4
wujie <wujie@polyv.net>
2020-11-04 12:36:43 +0800

    JSDK-1 自动脚本提交(版本升级、bug优化)

--- auto_depoly.bat
+++ auto_depoly.bat
@@ -4,7 +4,6 @@ git checkout V1.0.0
 git pull 
 git commit -am "JSDK-1 自动脚本提交(版本升级、bug优化)"  &&  git  push
 :: # 切换到master分支
-PAUSE
 git checkout master
 git pull origin master
 :: # 合并V1.0.0 分支代码
@@ -12,10 +11,11 @@ git merge V1.0.0
 git status
 :: # 提交master分支到origin远程仓库
 git push origin master
+:: # 部署到中央版本库
+:: mvn clean install deploy -Dmaven.test.skip=true -P release
+PAUSE
 :: # 提交master分支到github远程仓库
 git push github master
-:: # 部署到中央版本库
-mvn clean install deploy -Dmaven.test.skip=true -P release
 :: # 切换回V1.0.0分支
 git checkout V1.0.0
 

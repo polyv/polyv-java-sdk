825e6e0756c2843eb723358d2c504af81e44a93b
wujie <wujie@polyv.net>
2020-11-06 16:01:26 +0800

    JSDK-1  基础代码提交

--- polyv-java-live-sdk/src/main/java/net/polyv/live/entity/chat/LiveGetAccountBadWordRequest.java
+++ polyv-java-live-sdk/src/main/java/net/polyv/live/entity/chat/LiveGetAccountBadWordRequest.java
@@ -1,7 +1,6 @@
 package net.polyv.live.entity.chat;
 
 import io.swagger.annotations.ApiModel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -9,13 +8,11 @@ import net.polyv.live.entity.LiveCommonRequest;
 
 /**
  * 查询账号严禁词列表请求实体
- * @author: thomas
+ * @author  thomas
  **/
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
-@AllArgsConstructor
-//@NoArgsConstructor
 @ApiModel("查询账号严禁词列表请求实体")
 public class LiveGetAccountBadWordRequest extends LiveCommonRequest {
      

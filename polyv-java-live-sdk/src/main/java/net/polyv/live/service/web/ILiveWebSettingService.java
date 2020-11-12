ccf0e921b0bfb0e2ce4c9e249fbd17591cc49188
sadboy <wangzhikang@polyv.net>
2020-11-12 11:38:05 +0800

    JSDK-175 上传图片资源

--- polyv-java-live-sdk/src/main/java/net/polyv/live/service/web/ILiveWebSettingService.java
+++ polyv-java-live-sdk/src/main/java/net/polyv/live/service/web/ILiveWebSettingService.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 import net.polyv.live.entity.web.setting.LiveChannelGlobalSwitchRequest;
+import net.polyv.live.entity.web.setting.LiveUploadImageRequest;
+import net.polyv.live.entity.web.setting.LiveUploadImageResponse;
 
 /**
  * @author: sadboy
@@ -21,4 +23,14 @@ public interface ILiveWebSettingService {
     Boolean channelGlobalSwitch(LiveChannelGlobalSwitchRequest liveChannelGlobalSwitchRequest)
             throws IOException, NoSuchAlgorithmException;
     
+    /**
+     * 上传图片资源
+     * API地址：https://dev.polyv.net/2018/liveproduct/l-api/szgkygg/kjsz/uploadimage/
+     * @param liveUploadImageRequest 上传图片资源请求实体
+     * @return 上传图片资源返回实体
+     * @throws IOException 异常
+     * @throws NoSuchAlgorithmException 异常
+     */
+    LiveUploadImageResponse uploadImage(LiveUploadImageRequest liveUploadImageRequest)
+            throws IOException, NoSuchAlgorithmException;
 }

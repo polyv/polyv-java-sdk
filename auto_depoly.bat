fa627bb107222a80fe68277b69d06bf07c52f302
wujie <wujie@polyv.net>
2020-11-18 17:08:50 +0800

    JSDK-1  分支代码修改

--- auto_depoly.bat
+++ auto_depoly.bat
@@ -1,14 +1,14 @@
 :: @echo off
 
-git checkout V1.0.0
+git checkout dev
 :: #提交所有代码
 git pull 
 git commit -am "JSDK-1 auto_deploy"  &&  git  push
 :: # 切换到master分支
 git checkout master
 git pull origin master
-:: # 合并V1.0.0 分支代码
-git merge V1.0.0
+:: # 合并dev 分支代码
+git merge dev --no-ff
 git status
 :: # 提交master分支到origin远程仓库
 git push origin master
@@ -19,7 +19,6 @@ PAUSE
 git push github master
 :: # 提交master分支到gitee远程仓库
 git push gitee master
-:: # 切换回V1.0.0分支
-git checkout V1.0.0
-
+:: # 切换回dev分支
+git checkout dev
 PAUSE

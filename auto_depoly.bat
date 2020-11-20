b00ee3bf8e67335fbff70c0d0eb20ade0e750def
wujie <wujie@polyv.net>
2020-11-20 12:14:11 +0800

    JSDK-1    sonar 问题

--- auto_depoly.bat
+++ auto_depoly.bat
@@ -1,3 +1,7 @@
+:: 执行集成测试，确认测试没有问题全部通过
+:: mvn clean  test
+
+
 :: @echo off
 :: 调用示例  auto_deploy.bat  1.0.9
 git checkout dev
@@ -6,6 +10,7 @@ git pull
 :: git commit -am "JSDK-1 auto_deploy"  &&  git  push
 :: # 切换到master分支
 git checkout master
+PAUSE
 :: 同步主分支代码
 git pull origin master
 :: # 合并dev 分支代码
@@ -13,13 +18,8 @@ git merge dev --no-ff
 :: 查看同步状态，确认合并完成
 git status
 PAUSE
-:: 执行集成测试，确认测试没有问题全部通过
-:: mvn clean  test
-:: PAUSE
 :: 针对当前分支新建一个版本tag
 git  tag  %1
-:: # 部署到中央版本库
-:: mvn clean install deploy -Dmaven.test.skip=true -P release
 :: # 提交master分支到origin远程仓库
 git push origin master
 :: # 提交master分支到github远程仓库
@@ -30,6 +30,10 @@ git push gitee master
 git checkout dev
 PAUSE
 
+
+
 :: 剩余工作如下
+:: # 部署到中央版本库
+:: mvn clean install deploy -Dmaven.test.skip=true -P release
 :: 登录码云重启文档服务，使最新文档生效，并人工确认
 :: 登录 maven 私服更新到中央版本库 ， https://oss.sonatype.org/#stagingRepositories ，并人工确认

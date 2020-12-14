1a91aa8b6dc4e212156631d36bb87e6c19a5d3e4
sadboy <wangzhikang@polyv.net>
2020-12-14 11:14:24 +0800

    JSDK-203 加上必要的注释

--- polyv-java-sdk-common/src/main/java/net/polyv/common/v1/validator/handle/Validator.java
+++ polyv-java-sdk-common/src/main/java/net/polyv/common/v1/validator/handle/Validator.java
@@ -10,26 +10,54 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import net.polyv.common.v1.validator.ViolationMsg;
 
+/**
+ * 自定义验证器父类
+ */
 @Slf4j
 public abstract class Validator {
     
+    /**
+     * 下一个验证器，为null则为最后一个验证器
+     */
     private Validator nextRequestValidator;
     
+    /**
+     * 验证器对应注解的class
+     */
     protected Class currentClass;
     
+    /**
+     * 是否快速验证标识，true为快速验证，false为全部验证后返回
+     */
     private boolean fastFail = false;
     
+    /**
+     * 是否快速验证(一个验证不通过即返回)，true为快速验证，false为所有验证结束后统一返回，默认为false
+     * @param fastFail 快速验证
+     * @return
+     */
     public Validator setFastFail(boolean fastFail) {
         this.fastFail = fastFail;
         return this;
     }
     
+    /**
+     * 设置链式验证器下一个节点
+     * @param nextRequestValidator
+     */
     public void setNextRequestValidator(Validator nextRequestValidator) {
         this.nextRequestValidator = nextRequestValidator;
     }
     
+    /**
+     * 验证data数据是否满足数据要求
+     * @param data 数据
+     * @param groups 需要验证的分组
+     * @return
+     */
     public final List<ViolationMsg> validate(Object data, Class<?>... groups) {
         try {
+            groups = groups == null ? new Class[]{} : groups;
             Class<?> objClass = data.getClass();
             List<ViolationMsg> violationMsgList = new ArrayList<ViolationMsg>();
             while (null != objClass) {
@@ -61,6 +89,14 @@ public abstract class Validator {
         return new ArrayList<ViolationMsg>();
     }
     
+    /**
+     * 链式验证，如果不属于当前验证器，调用nextRequestValidator去验证
+     * @param annotation 注解对象
+     * @param field 验证的字段
+     * @param data 字段所对应的数据
+     * @param groups 需要验证的分组，不能为空
+     * @return
+     */
     protected final List<ViolationMsg> validate(Annotation annotation, Field field, Object data, Class<?>... groups) {
         List<ViolationMsg> violationMsgList = new ArrayList<ViolationMsg>();
         if (null != currentClass && currentClass.equals(annotation.annotationType())) {
@@ -78,9 +114,19 @@ public abstract class Validator {
         return violationMsgList;
     }
     
+    /**
+     * 每一个验证器需要处理的逻辑
+     * @param annotation 注解对象
+     * @param data 字段对应的数据
+     * @param groups 需要验证的组，验证所有组不传该参数即可
+     * @return
+     */
     abstract protected String dealValidate(Annotation annotation, Object data, Class<?>... groups);
     
-    
+    /**
+     * 获取验证器链表
+     * @return 返回验证器链表第一个元素
+     */
     public static Validator getValidator() {
         LengthValidator lengthValidator = new LengthValidator();
         MinValidator minValidator = new MinValidator();
@@ -125,4 +171,5 @@ public abstract class Validator {
         set.retainAll(Arrays.asList(groupsTwo));
         return set.size() > 0;
     }
+    
 }

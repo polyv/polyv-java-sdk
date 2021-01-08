5097c497df41c34ce4fdfc1cd9b3e9cad06a55d0
sadboy <wangzhikang@polyv.net>
2021-01-08 15:53:57 +0800

    JSDK-2 修复注释不正确的问题

--- polyv-java-live-sdk/src/main/java/net/polyv/live/v1/entity/player/LiveSetWarmupVedioRequest.java
+++ polyv-java-live-sdk/src/main/java/net/polyv/live/v1/entity/player/LiveSetWarmupVedioRequest.java
@@ -28,7 +28,7 @@ public class LiveSetWarmupVedioRequest extends LiveCommonRequest {
     
     
     /**
-     * 开关值，Y或N，Y表示开启，N表示关闭
+     * 暖场视频地址(http地址)，移动端不支持FLV视频文件，建议使用MP4视频文件
      */
     @ApiModelProperty(name = "warmUpFlv", value = "暖场视频地址(http地址)，移动端不支持FLV视频文件，建议使用MP4视频文件", required = true)
     @NotNull(message = "属性warmUpFlv不能为空")

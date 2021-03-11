1616a595a804a84fd321aa4945301f19478dfe63
伍杰 <wujie@polyv.net>
2021-03-11 15:06:03 +0800

    JSDK-1   代码微调

--- polyv-java-vod-sdk/src/main/java/net/polyv/vod/v1/service/account/IVodAccountService.java
+++ polyv-java-vod-sdk/src/main/java/net/polyv/vod/v1/service/account/IVodAccountService.java
@@ -10,7 +10,13 @@ import net.polyv.vod.v1.entity.account.VodAccountSpaceDataResponse;
  * @author: thomas
  **/
 public interface IVodAccountService {
-
+     /**
+      * 获取用户空间及流量情况 , API地址：https://dev.polyv.net/2013/videoproduct/v-api/v-api-main/getspaceandflow/
+      * @param vodAccountSpaceDataRequest 获取用户空间及流量情况请求实体
+      * @return 获取用户空间及流量详细数据
+      * @throws IOException IO异常
+      * @throws NoSuchAlgorithmException 签名算法异常
+      */
      VodAccountSpaceDataResponse getAccountSpaceFlow(VodAccountSpaceDataRequest vodAccountSpaceDataRequest)
              throws IOException, NoSuchAlgorithmException;
 

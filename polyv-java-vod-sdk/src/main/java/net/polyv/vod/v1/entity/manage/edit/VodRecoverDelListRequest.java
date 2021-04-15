ebea2ba5138231fc6a3b6a37f01e934e078f231c
sadboy <wangzhikang@polyv.net>
2021-04-15 10:09:34 +0800

    JSDK-225 完成【点播】恢复回收站视频API接口

--- polyv-java-vod-sdk/src/main/java/net/polyv/vod/v1/entity/manage/edit/VodRecoverDelListRequest.java
+++ polyv-java-vod-sdk/src/main/java/net/polyv/vod/v1/entity/manage/edit/VodRecoverDelListRequest.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import net.polyv.common.v1.validator.constraints.NotNull;
 import net.polyv.vod.v1.entity.VodCommonRequest;
 
 /**
@@ -23,6 +24,7 @@ public class VodRecoverDelListRequest extends VodCommonRequest {
      * 点播视频videoId，多个使用英文逗号分隔
      */
     @ApiModelProperty(name = "videoIds", value = "点播视频videoId，多个使用英文逗号分隔", required = true)
+    @NotNull(message = "属性videoIds不能为空")
     @JSONField(name = "vids")
     private String videoIds;
 

0c95f5aef63cdd6e1feb1df143e993cd389e9862
fangyan <fangyan@polyv.net>
2021-05-10 16:00:25 +0800

    JSDK-1 解决视频播放量统计接口日期序列化异常

--- polyv-java-vod-sdk/src/main/java/net/polyv/vod/v1/entity/datastatistics/VodQueryVideoPlaybackStatisticsResponse.java
+++ polyv-java-vod-sdk/src/main/java/net/polyv/vod/v1/entity/datastatistics/VodQueryVideoPlaybackStatisticsResponse.java
@@ -1,9 +1,5 @@
 package net.polyv.vod.v1.entity.datastatistics;
 
-import java.util.Date;
-
-import com.alibaba.fastjson.annotation.JSONField;
-
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -18,11 +14,10 @@ import lombok.experimental.Accessors;
 @ApiModel("查询视频播放量统计数据返回实体")
 public class VodQueryVideoPlaybackStatisticsResponse {
     /**
-     * 当前日期，格式为：yyyy-MM-dd
+     * 当前日期，格式为：yyyy-MM-dd或yyyy-MM
      */
-    @ApiModelProperty(name = "currentTime", value = "当前日期，格式为：yyyy-MM-dd", required = false)
-    @JSONField(format = "yyyy-MM-dd")
-    private Date currentTime;
+    @ApiModelProperty(name = "currentTime", value = "当前日期，格式为：yyyy-MM-dd或yyyy-MM", required = false)
+    private String currentTime;
     
     /**
      * pc端播放量
